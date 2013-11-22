3d8f58378a32cba374697958eedb1d2700226c17
Trond Arve Nordheim <trond@nordheim.io>
2013-11-23 00:14:49 +0100

    Initial commit

---
+++ WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System.Configuration;
+using System.Web.Mvc;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.ServiceRuntime;
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+        public ActionResult Index()
+        {
+            var value = GetTestValue();
+            return Content(string.Format("Retrieved value: {0}", value));
+        }
+
+        private string GetTestValue()
+        {
+            if (RoleEnvironment.IsAvailable && !RoleEnvironment.IsEmulated)
+            {
+                return CloudConfigurationManager.GetSetting("test_setting");
+            }
+            return ConfigurationManager.AppSettings["test_setting"];
+        }
+	}
+}
